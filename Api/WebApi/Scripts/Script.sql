f41abfaaa4dcc3651f62afeac817ff8eebd59c9a
emmy <104332292+emmillylins@users.noreply.github.com>
2023-05-13 12:21:53 -0300

    Add files via upload

---
+++ Api/WebApi/Scripts/Script.sql
@@ -0,0 +1,29 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Users] (
+    [Id] int NOT NULL IDENTITY,
+    [Login] VARCHAR(100) NOT NULL,
+    [Password] VARCHAR(100) NOT NULL,
+    [Email] VARCHAR(250) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230510161346_Mig', N'7.0.5');
+GO
+
+COMMIT;
+GO
+
---
+++ Api/WebApi/Startup.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using Domain.Interfaces;
+using Domain.Services;
+using UserClass.Entities;
+using Data.Context;
+using Data.Repository;
+using WebApi.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var server = Configuration["database:mysql:server"];
+            var database = Configuration["database:mysql:database"];
+            services.AddControllers();
+            services.AddDbContext<DataDbContext>(options =>
+            {
+                options.UseSqlServer(@"data source=(localdb)\Db;initial catalog=DataBase;TrustServerCertificate=True;Integrated Security=SSPI", opt =>
+                {
+                    opt.MigrationsAssembly("WebApi");
+                    opt.CommandTimeout(180);
+                    opt.EnableRetryOnFailure(5);
+                });
+            });
+
+            services.AddScoped<IBaseRepository<User>, BaseRepository<User>>();
+            services.AddScoped<IBaseService<User>, BaseService<User>>();
+
+            services.AddSingleton(new MapperConfiguration(config =>
+            {
+                config.CreateMap<CreateUserDto, User>();
+                config.CreateMap<User, UserDto>();
+                config.CreateMap<UserDto, User>();
+            }).CreateMapper());
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors(options => options.AllowAnyOrigin());
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
